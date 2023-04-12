9821e5e8a82b00f21b835428106a0c8ce06183c3
hdert <40216616+hdert@users.noreply.github.com>
2023-04-12 21:47:38 +1200

    remove comments containing dead code

--- src/main.rs
+++ src/main.rs
@@ -81,8 +81,6 @@ async fn main() {
     let bypass_token = env_file.get("BYPASS_TOKEN").unwrap();
 
     debug!("Grabbed tokens");
-    // let (zone_identifier, record_identifier, env_file) =
-    // get_zone_record_identifiers(&mut env_file, token, &config_file);
     let (zone_identifier, record_identifier) =
         match (env_file.get("ZONE_ID"), env_file.get("RECORD_ID")) {
             (Some(result_1), Some(result_2)) => {
@@ -128,8 +126,7 @@ async fn main() {
     {
         return;
     }
-    // let (account_identifier, group_identifier) =
-    // get_account_group_identifiers(token, env_file, &config_file);
+
     let account_identifier = env_file.get("ACCOUNT_ID").expect("Error: No account ID");
     let group_identifier = match env_file.get("GROUP_ID") {
         Some(result) => {
@@ -141,74 +138,17 @@ async fn main() {
             get_group_id_from_cloudflare(
                 bypass_token,
                 account_identifier,
-                config_file.get("GROUP_NAME").unwrap(),
+                config_file
+                    .get("GROUP_NAME")
+                    .expect("Error: GROUP_NAME not set"),
             )
             .await
             .unwrap()
-            // get_account_group_ids_from_cloudflare(
-            //     bypass_token,
-            //     config_file
-            //         .get("ACCOUNT_NAME")
-            //         .expect("Error: ACCOUNT_NAME not set"),
-            //     config_file
-            //         .get("GROUP_NAME")
-            //         .expect("Error: GROUP_NAME not set"),
-            // )
         }
     };
     debug!("Account id: {account_identifier}");
     debug!("Group id: {group_identifier}");
-
-    // println!("{zone_identifier} {record_identifier}");
-    // println!("{account_identifier} {group_identifier}");
 }
-
-// fn get_zone_record_identifiers<'a>(
-//     env_file: &'a mut EnvFile,
-//     token: &str,
-//     config_file: &EnvFile,
-// ) -> (String, String, &'a mut EnvFile) {
-//     debug!("Entered get_zone_record_identifiers");
-//     let zone_id = env_file.get("ZONE_ID");
-//     let record_id = env_file.get("RECORD_ID");
-//     debug!("Zone id prelim: {zone_id:#?}");
-//     debug!("Record id prelim: {record_id:#?}");
-
-//     match (zone_id, record_id) {
-//         (Some(result_1), Some(result_2)) => {
-//             debug!("From file was valid");
-//             (result_1.to_string(), result_2.to_string(), env_file)
-//         }
-//         (Some(result_1), None) => {
-//             debug!("Left valid from file");
-//             (
-//                 result_1.to_string(),
-//                 get_record_id_from_cloudflare(
-//                     result_1,
-//                     config_file
-//                         .get("RECORD_NAME")
-//                         .expect("Error: RECORD_NAME not set."),
-//                     token,
-//                 ),
-//                 env_file,
-//             )
-//         }
-//         (None, None) | (None, Some(_)) => {
-//             debug!("None valid");
-//             match get_zone_record_ids_from_cloudflare(
-//                 token,
-//                 config_file
-//                     .get("ZONE_NAME")
-//                     .expect("Error: ZONE_NAME not set"),
-//                 config_file
-//                     .get("RECORD_NAME")
-//                     .expect("Error: RECORD_NAME not set."),
-//             ) {
-//                 (result_1, result_2) => (result_1, result_2, env_file),
-//             }
-//         }
-//     }
-// }
 
 async fn get_zone_record_ids_from_cloudflare(
     token: &str,
@@ -266,64 +206,6 @@ async fn cloudflare_get(token: &str, url: String) -> Result<CloudflareResponse, 
     Ok(response)
 }
 
-// fn get_account_group_identifiers(
-//     token: &str,
-//     env_file: &mut EnvFile,
-//     config_file: &EnvFile,
-// ) -> (String, String) {
-//     debug!("Entered get_account_group_identifiers");
-//     let account_id = env_file.get("ACCOUNT_ID");
-//     let group_id = env_file.get("GROUP_ID");
-//     debug!("Account id prelim: {account_id:#?}");
-//     debug!("Group id prelim: {group_id:#?}");
-
-//     match (account_id, group_id) {
-//         (Some(result_1), Some(result_2)) => {
-//             debug!("From file was valid");
-//             (result_1.to_string(), result_2.to_string())
-//         }
-//         (Some(result), None) => {
-//             debug!("Left valid from file");
-//             (
-//                 result.to_string(),
-//                 get_group_id_from_cloudflare(token, result, config_file.get("GROUP_NAME").unwrap()),
-//             )
-//         }
-//         (None, None) | (None, Some(_)) => {
-//             debug!("None valid");
-//             get_account_group_ids_from_cloudflare(
-//                 token,
-//                 config_file
-//                     .get("ACCOUNT_NAME")
-//                     .expect("Error: ACCOUNT_NAME not set"),
-//                 config_file
-//                     .get("GROUP_NAME")
-//                     .expect("Error: GROUP_NAME not set"),
-//             )
-//         }
-//     }
-// }
-
-// fn get_account_group_ids_from_cloudflare(
-//     token: &str,
-//     account_name: &str,
-//     group_name: &str,
-// ) -> (String, String) {
-//     debug!("Entered get_account_group_ids_from_cloudflare");
-//     let response = cloudflare_get(
-//         token,
-//         "https://api.cloudflare.com/client/v4/accounts".to_string(),
-//     );
-//     // let account_id = response["result"][0]["id"].as_str().unwrap();
-//     let account_id = "placeholder1";
-//     debug!("Account group ids response: {response:#?}");
-//     debug!("Account id is: {account_id:#?}");
-//     (
-//         account_id.to_string(),
-//         get_group_id_from_cloudflare(token, account_id, group_name),
-//     )
-// }
-
 async fn get_group_id_from_cloudflare(
     token: &str,
     account_id: &str,
@@ -344,15 +226,6 @@ async fn get_group_id_from_cloudflare(
         .as_str()
         .unwrap()
         .to_owned())
-    // let v = response["result"].as_array().unwrap().clone();
-    // let mut result: String = format!("Uninitialized");
-    // for i in v {
-    //     debug!("{i:#?}");
-    //     if i["name"].as_str().unwrap() == group_name {
-    //         return Some(i["id"].as_str().unwrap().to_string());
-    //     }
-    // }
-    // return None;
 }
 
 async fn get_ip() -> Result<String, reqwest::Error> {
